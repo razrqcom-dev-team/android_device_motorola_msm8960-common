ffbf6ae0ddefa338f93db5f85cf7f99f90f5c050
pcarenza <pcarenza@gmail.com>
2013-05-04 15:26:52 -0400

    remove p2p overlay

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -33,6 +33,7 @@ TARGET_CPU_SMP := true
 COMMON_GLOBAL_CFLAGS += -D__ARM_USE_PLD -D__ARM_CACHE_LINE_SIZE=64
 TARGET_GLOBAL_CFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
 TARGET_GLOBAL_CPPFLAGS += -mfpu=neon-vfpv4 -mfloat-abi=softfp
+TARGET_EXTRA_CFLAGS:= -mtune=cortex-a9 -mcpu=cortex-a9
 BOARD_USES_QCOM_HARDWARE := true
 
 # Krait optimizations

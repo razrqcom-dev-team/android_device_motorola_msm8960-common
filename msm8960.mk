8cfcfa283522feb399472b7b89fc9b410c7173f9
dhacker29 <dhackerdvm@gmail.com>
2015-01-13 03:15:41 -0500

    Recovery: Fix copy of assets
    Change-Id: I2676b1471193df351563d09c798ac0d08198911b

--- msm8960.mk
+++ msm8960.mk
@@ -115,7 +115,7 @@ endif
 
 # TWRP
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/twrp.fstab:root/etc/twrp.fstab
+    $(LOCAL_PATH)/rootdir/etc/twrp.fstab:recovery/root/etc/twrp.fstab
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
